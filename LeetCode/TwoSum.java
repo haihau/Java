8a4e565929ab8d23305449f0868d633b7aa9d18b
Shih Liang Fang <37144635+haihau@users.noreply.github.com>
2019-12-02 19:21:58 +0930

    LeetCode - Day 2 two sum

---
+++ LeetCode/TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SweetJava {
+       public static void main(String[] args){
+           int[] nums = {2,8,9,12};
+           System.out.println(Arrays.toString(twoSum(nums, 9)));
+       }
+
+       public static int[] twoSum(int[] nums, int target) {
+             for (int i = 0; i < nums.length; i++) {
+                 for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] + nums[i] == target) {
+                       int [] x = new int[] { i, j };
+                       return x;
+                    }
+                 }
+             }
+             int y [] = {0,0};
+           return y;
+       }
+}
